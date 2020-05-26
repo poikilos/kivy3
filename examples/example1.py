4fae0dbed6d85f9220e88e7c47d94c864ff5ded4
Robert Niederreiter <office@squarewave.at>
2020-05-26 09:57:25 +0200

    Cleanup base examples. (#12)

--- examples/example1.py
+++ examples/example1.py
@@ -1,39 +1,10 @@
-"""
-The MIT License (MIT)
-
-Copyright (c) 2014 Niko Skrypnik
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-"""
-
-"""
-This example loads simple objects from .obj file and shows how
-to use custom shader file
-"""
-
-import os
-import kivy3
 from kivy.app import App
-from kivy3 import Scene, Renderer, PerspectiveCamera
-from kivy3.loaders import OBJLoader
 from kivy.uix.floatlayout import FloatLayout
-
+from kivy3 import PerspectiveCamera
+from kivy3 import Renderer
+from kivy3 import Scene
+from kivy3.loaders import OBJLoader
+import os
 
 # Resources pathes
 _this_path = os.path.dirname(os.path.realpath(__file__))
@@ -42,22 +13,28 @@ obj_path = os.path.join(_this_path, "./testnurbs.obj")
 
 
 class MainApp(App):
+    """This example loads simple objects from .obj file and shows how
+    to use custom shader file
+    """
+
     def build(self):
-        root = FloatLayout()
-        self.renderer = Renderer(shader_file=shader_file)
-        scene = Scene()
-        camera = PerspectiveCamera(15, 1, 1, 1000)
         # load obj file
         loader = OBJLoader()
         obj = loader.load(obj_path)
 
+        scene = Scene()
         scene.add(*obj.children)
         for obj in scene.children:
             obj.pos.z = -20
 
-        self.renderer.render(scene, camera)
-        root.add_widget(self.renderer)
-        self.renderer.bind(size=self._adjust_aspect)
+        camera = PerspectiveCamera(15, 1, 1, 1000)
+
+        renderer = self.renderer = Renderer(shader_file=shader_file)
+        renderer.bind(size=self._adjust_aspect)
+        renderer.render(scene, camera)
+
+        root = FloatLayout()
+        root.add_widget(renderer)
         return root
 
     def _adjust_aspect(self, inst, val):
--- examples/example2.py
+++ examples/example2.py
@@ -1,51 +1,24 @@
-"""
-The MIT License (MIT)
-
-Copyright (c) 2014 Niko Skrypnik
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-"""
-
-"""
-Same as example1 but with using default shader file and colorizing
-of the objects
-"""
-
-import os
-import kivy3
 from kivy.app import App
-from kivy3 import Scene, Renderer, PerspectiveCamera
-from kivy3.loaders import OBJLoader
 from kivy.uix.floatlayout import FloatLayout
+from kivy3 import PerspectiveCamera
+from kivy3 import Renderer
+from kivy3 import Scene
+from kivy3.loaders import OBJLoader
+import os
 
 
 class MainApp(App):
+    """Same as example1 but with using default shader file and colorizing
+    of the objects
+    """
+
     def build(self):
-        root = FloatLayout()
-        self.renderer = Renderer()
-        scene = Scene()
-        camera = PerspectiveCamera(15, 1, 1, 1000)
         # load obj file
         loader = OBJLoader()
         obj_path = os.path.join(os.path.dirname(__file__), "./testnurbs-2.obj")
         obj = loader.load(obj_path)
 
+        scene = Scene()
         scene.add(*obj.children)
         for obj in scene.children:
             obj.pos.z = -20
@@ -62,9 +35,14 @@ class MainApp(App):
         scene.children[2].material.diffuse = 0.0, 0.0, 0.7  # blue
         scene.children[3].material.diffuse = 0.7, 0.7, 0.0  # yellow
 
-        self.renderer.render(scene, camera)
-        root.add_widget(self.renderer)
-        self.renderer.bind(size=self._adjust_aspect)
+        camera = PerspectiveCamera(15, 1, 1, 1000)
+
+        renderer = self.renderer = Renderer()
+        renderer.bind(size=self._adjust_aspect)
+        renderer.render(scene, camera)
+
+        root = FloatLayout()
+        root.add_widget(renderer)
         return root
 
     def _adjust_aspect(self, inst, val):
